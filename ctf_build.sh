013f383a565024aefab9df2ef5d17737218e2549
npmaharaj1 <109418510+npmaharaj1@users.noreply.github.com>
2024-05-22 18:12:36 +1000

    Update ctf_build.sh

--- ctf_build.sh
+++ ctf_build.sh
@@ -16,9 +16,9 @@ printf "${RED}Cloning Respository${NC}\n"
 git clone https://github.com/npmaharaj1/ctf
 
 printf "${RED}Copying Files${NC}\n"
-cp ctf/admin /var/www/html
-cp ctf/index.html /var/www/html
+cp -r ctf/admin /var/www/html
+cp -r ctf/index.html /var/www/html
 
 printf "${RED}Setting up User${NC}\n"
-adduseradd -m stevenirate -p georgia
+useradd -m stevenirate -p georgia
 echo "a7ba7425c4366547f458ac75cba3e4b4" > /home/stevenirate/flag.txt
