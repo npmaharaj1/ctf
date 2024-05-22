1cb41bb8bfd5750b000eb40dcbac198a51807ee3
npmaharaj1 <109418510+npmaharaj1@users.noreply.github.com>
2024-05-22 18:30:03 +1000

    Update attack_build.sh

--- attack_build.sh
+++ attack_build.sh
@@ -12,7 +12,5 @@ clear
 printf "${RED}Installing Dependencies${NC}\n"
 apt update
 apt install -y nmap ffuf hydra
-mkdir ctf_tools/
-cd ctf_tools/
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 wget https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt
